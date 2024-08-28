794bf1f2547936cf34ccd1a2b95488e658f95add
Alex Suraykin <alexsuraykin@gmail.com>
2024-08-28 23:40:11 +0300

    All files to github

---
+++ go.mod
@@ -0,0 +1,3 @@
+module skillfactory/25.7.1
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	var n string
+	fmt.Print("Введите данные: ")
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Вы ввели следующие данные: %s\n", n)
+}
